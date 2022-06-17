b5d83a7e92f2954366387ab20ab3168f1f144254
Alejandro Alvarez <alex.david.alvarez.sosa@gmail.com>
2022-06-16 22:09:07 -0400

    Added fresh environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  tmdbApiToken: 'YOUR_TOKEN_HERE',
+  tmdbUrl: 'https://api.themoviedb.org/3',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  tmdbApiToken: 'YOUR_TOKEN_HERE',
+  tmdbUrl: 'https://api.themoviedb.org/3',
+};
